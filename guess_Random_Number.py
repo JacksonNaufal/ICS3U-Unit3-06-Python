fcb608bde48212e87a5d6746f13839f0a7035a98
Jackson Naufal <Jackson.naufal@mths.ca>
2022-04-02 14:13:19 +0000

    guess_Random_number

--- guess_Random_Number.py
+++ guess_Random_Number.py
@@ -9,21 +9,25 @@ import random
 
 def main():
 
-    # This is a random number guesser
+    # This is a random number guesser, with try and catch
 
     # input
-    guess_Number = int(input("Enter your first guess here (0-9): "))
-
+    guess_number_string = input("Enter your first guess here (0-9): ")
     random_Number = random.randint(0, 9)  # a number between 0 and 9
 
     # process & output
-    if guess_Number == random_Number:
-        print("\nGuess is correct!")
-        print("\nDone!")
-    else:
-        print("\nYour guess is incorrect!")
-        print("\nThe correct number is {0}.".format(random_Number))
-        print("\nDone.")
+    try:
+        guess_Number = int(guess_number_string)
+
+        if guess_Number == random_Number:
+            print("\nGuess is correct!")
+        else:
+            print("You guessed incorrectly")
+            print("\nThe correct number is {0}.".format(random_Number))
+
+    except Exception:
+        print("\nThat was not an integer")
+    print("\nDone.")
 
 
 if __name__ == "__main__":
